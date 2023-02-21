542d263a9723a9ecf2ced85366d33ba207e3f148
joshjh2002 <joshhelman@hotmail.co.uk>
2023-02-21 23:38:01 +0000

    Added landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase.js";
 
 export default function Welcome() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignin = () => {
+    signInWithEmailAndPassword(auth, email, password).catch((err) =>
+      alert(err.message)
+    );
+  };
+
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>To-Do List</h1>
+      <div>
+        <input type="email" onChange={handleEmailChange} value={email} />
+        <input
+          type="password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <button onClick={handleSignin}>Sign In</button>
+      </div>
     </div>
   );
 }
