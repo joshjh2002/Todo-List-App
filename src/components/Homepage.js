971484642fd7656d980e02294cfacf31e1a98981
joshjh2002 <joshhelman@hotmail.co.uk>
2023-02-22 14:50:46 +0000

    Added login and signup features

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Welcome() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (!user) navigate("/");
+    });
+  });
+
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <div>
       <h1>Homepage</h1>
+      <button onClick={handleSignout}>Sign Out</button>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase.js";
+import { useNavigate } from "react-router-dom";
 
 export default function Welcome() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState("");
+  const [registerInformation, setRegisterInformation] = useState({
+    email: "",
+    password: "",
+    confirmEmail: "",
+    confirmPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate("/homepage");
+      }
+    });
+  });
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,22 +36,112 @@ export default function Welcome() {
   };
 
   const handleSignin = () => {
-    signInWithEmailAndPassword(auth, email, password).catch((err) =>
-      alert(err.message)
-    );
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate("/homepage");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  const handleRegister = () => {
+    if (
+      registerInformation.email !== registerInformation.confirmEmail ||
+      registerInformation.password !== registerInformation.confirmPassword
+    ) {
+      alert("Please confirm credentials are the same!");
+      return;
+    }
+
+    signInWithEmailAndPassword(
+      auth,
+      registerInformation.email,
+      registerInformation.password
+    )
+      .then(() => {
+        navigate("/homepage");
+      })
+      .catch((err) => alert(err.message));
   };
 
   return (
     <div>
       <h1>To-Do List</h1>
       <div>
-        <input type="email" onChange={handleEmailChange} value={email} />
-        <input
-          type="password"
-          onChange={handlePasswordChange}
-          value={password}
-        />
-        <button onClick={handleSignin}>Sign In</button>
+        {isRegistering ? (
+          <>
+            <input
+              type="email"
+              placeholder="Email"
+              value={registerInformation.email}
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  email: e.target.value,
+                })
+              }
+            />
+            <input
+              type="email"
+              placeholder="Confirm Email"
+              value={registerInformation.confirmEmail}
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  confirmEmail: e.target.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={registerInformation.password}
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  password: e.target.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={registerInformation.confirmPassword}
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  confirmPassword: e.target.value,
+                })
+              }
+            />
+            <button onClick={handleRegister}>Sign Up</button>
+            <button
+              onClick={() => {
+                setIsRegistering(false);
+              }}
+            >
+              Back to Log In
+            </button>
+          </>
+        ) : (
+          <>
+            <input type="email" onChange={handleEmailChange} value={email} />
+            <input
+              type="password"
+              onChange={handlePasswordChange}
+              value={password}
+            />
+            <button onClick={handleSignin}>Sign In</button>
+            <button
+              onClick={() => {
+                setIsRegistering(true);
+              }}
+            >
+              Create Account
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
