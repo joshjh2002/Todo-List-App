8e9c91db4f007ab86be61eae2fe6891af53f8d33
joshjh2002 <joshhelman@hotmail.co.uk>
2023-02-22 22:13:47 +0000

    Add items to todo list

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { uid } from "uid";
+import { set, ref, onValue } from "firebase/database";
 
 export default function Welcome() {
+  const [todo, setTodo] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,9 +24,25 @@ export default function Welcome() {
         alert(err.message);
       });
   };
+
+  const writeToDatabase = () => {
+    const uidd = uid();
+    set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+      todo: todo,
+      uidd: uidd,
+    });
+    setTodo("");
+  };
+
   return (
     <div>
       <h1>Homepage</h1>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={writeToDatabase}>Add Item</button>
       <button onClick={handleSignout}>Sign Out</button>
     </div>
   );
